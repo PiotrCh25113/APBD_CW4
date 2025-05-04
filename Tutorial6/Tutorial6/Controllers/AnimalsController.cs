c3d70235850053d822863af098cda1431969ef25
PiotrCh25113 <s25113@pjwstk.edu.pl>
2025-05-04 21:11:03 +0200

    ImplementedAnimalMethods

---
+++ Tutorial6/Tutorial6/Controllers/AnimalsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tutorial6.Models;
+namespace Tutorial6.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AnimalsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var animals = Database.Animals;
+        return Ok(animals);
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var animal = Database.Animals.FirstOrDefault(x => x.Id == id);
+        return Ok(animal);
+    }
+    [HttpPost]
+    // POST api/animals
+    /*{
+        "id": 11,
+        "name": "TestAnimal",
+        "category": "TestCategory",
+        "weight": 3.2,
+        "color": "test-color"
+    }*/
+    public IActionResult Add(Animal animal)
+    {
+        Database.Animals.Add(animal);
+        return Created();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Animal animal)
+    {
+        int index = Database.Animals.FindIndex(a => a.Id == id);
+        if (index == -1)
+        {
+            return NotFound();
+        }
+
+        Database.Animals[index] = animal;
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var animal = Database.Animals.FirstOrDefault(x => x.Id == id);
+        Database.Animals.Remove(animal);
+        return Ok();
+    }
+    
+    [HttpGet("name/{name}")]
+    public IActionResult GetByName(String name)
+    {
+        var animal = Database.Animals.FirstOrDefault(x => x.Name == name);
+        return Ok(animal);
+    }
+}
--- Tutorial6/Tutorial6/Database.cs
+++ Tutorial6/Tutorial6/Database.cs
@@ -10,4 +10,20 @@ public static class Database
         new Test() { Id = 2, Name = "Test2" },
         new Test() { Id = 3, Name = "Test3" }
     };
+
+    public static List<Animal> Animals = new List<Animal>()
+    {
+        new Animal() { Id = 1, Name = "Garfield", Category = "Cat", Weight = 10, Color = "orange" },
+        new Animal() { Id = 2, Name = "Bella", Category = "Dog", Weight = 25, Color = "brown" },
+        new Animal() { Id = 3, Name = "Chirpy", Category = "Bird", Weight = 0.5, Color = "yellow" },
+        new Animal() { Id = 4, Name = "Nemo", Category = "Fish", Weight = 0.2, Color = "orange-white" },
+        new Animal() { Id = 5, Name = "Thumper", Category = "Rabbit", Weight = 2.5, Color = "gray" },
+        new Animal() { Id = 6, Name = "Zazu", Category = "Bird", Weight = 1.1, Color = "blue-red" },
+        new Animal() { Id = 7, Name = "Shadow", Category = "Cat", Weight = 9.5, Color = "black" },
+        new Animal() { Id = 8, Name = "Luna", Category = "Dog", Weight = 30, Color = "gray" },
+        new Animal() { Id = 9, Name = "Bella", Category = "Rabbit", Weight = 2.3, Color = "white" }, 
+        new Animal() { Id = 10, Name = "Sunny", Category = "Bird", Weight = 0.6, Color = "yellow" } 
+    };
+
+
 }
